eae6479fdfcdf97a37b97e8c8da5e19a94d98ca2
Naron <naron.chen@richmond.edu>
2023-08-30 01:42:37 -0400

    set ENV, added CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express') //import Express library
 const app = express() //creates a new Express application
 
+const cors = require('cors');
+
+// Allow only certain origins, adjust as needed
+app.use(cors({
+  origin: ['http://localhost:5000', 'http://localhost:5173']
+}));
+
 app.get("/api", (req,res) => {
     res.json({"users":["userOne", "userTwo", "userThree", "usrFour"]})
 })
