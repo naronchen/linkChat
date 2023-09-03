e371d4a605389a175cb0a0c443a743b6f3653264
Naron <naron.chen@richmond.edu>
2023-09-03 00:34:48 -0400

    properly format inputbox with icon

--- client/src/components/UserInput.jsx
+++ client/src/components/UserInput.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import '../styles/UserInput.css'
+import sendImg from '../assets/send.png'
 
 // @TODO responsive height on user input
 function UserInput({inputValue, onChange}) {
   return (
     <div className="input-box">
+        <div className="input-container">
         <input className="input-text" 
                 type="text" 
                 placeholder="Type something..."
                 value={inputValue}
                 onChange={(e) => onChange(e.target.value)}
                 />
+        <img  src={sendImg} 
+            alt="Send" 
+            className="send-img"/>
+
+</div>  
     </div>
   )
 }
