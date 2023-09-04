015591e559993773b019affd7818b32e36026fb9
Naron <naron.chen@richmond.edu>
2023-09-03 23:52:47 -0400

    forgot to add design doc here

--- mockAI/server.js
+++ mockAI/server.js
@@ -8,7 +8,12 @@ app.post('/mockAI', (req, res) => {
   res.json({ output: 'Hello from mockAI responding to: ' + `${req.body.input}` });
 });
 
+app.get("/apiTest", (req,res) => {
+  res.json({"users":["userOne", "userTwo", "userThree", "usrFour"]})
+})
+
 // Run Mock server
 app.listen(9000, () => {
   console.log('Mock AI server running on port 9000');
 });
+
